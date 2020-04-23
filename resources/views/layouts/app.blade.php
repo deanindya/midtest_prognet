3c49edf320f054de9274e501d7bf3baa861f9bdb
deanindya <deanindya@users.noreply.github.com>
2020-04-23 22:55:59 +0700

    show data category and todo at welcome page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Toedoe</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="/app.css" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+
+            .img-rounded{
+                width: 80px;
+                border-radius: 50%;
+            }
+
+            .text-dark a{
+                color: #636b6f;
+                text-decoration: none !important
+            }
+        </style>
+    </head>
+    <body>
+        <div class="col-lg-6 offset-lg-3">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container">
+                    <a class="navbar-brand" href="/">Toedoe</a>
+                </div>
+            </nav>
+
+            @yield('content')
+        </div>
+
+        
+    </body>
+
+    <script src="/jquery.js"></script>
+    <script src="/app.js"></script>
+</html>
